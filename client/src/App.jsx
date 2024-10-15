76e5c612b51faa08659e8d8186cf5a1cb39101bd
parth-vaghela005 <vaghelaparth300@gmail.com>
2024-10-15 21:23:17 +0530

    setu routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { Button } from './components/ui/button'
-import { Input } from './components/ui/input'
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import AuthLogin from "./pages/auth/Login";
+import AuthRegister from "./pages/auth/Registration";
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="car">
-        <Button  onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <Input type="text"/>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<div>
+      <Routes>
+        <Route
+          path="/auth"
+          element={
+            // <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+              <AuthLayout />
+            // </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
+      </Routes>
+    </div>
   )
 }
-
 export default App
